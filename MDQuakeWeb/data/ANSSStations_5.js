389f887351cf0650f1d9c44a63f3677d6348e047
bweiss1 <60404846+bweiss1@users.noreply.github.com>
2020-05-06 00:40:41 -0400

    upload webmap final

---
+++ MDQuakeWeb/data/ANSSStations_5.js
@@ -0,0 +1,10 @@
+var json_ANSSStations_5 = {
+"type": "FeatureCollection",
+"name": "ANSSStations_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Network Code": "US", "Station Code": "CBN", "Name": "Corbin (Fredericksburg Observatory), Virginia, USA", "Latitude": 38.205, "Longitude": -77.373, "Elevation": 70.0 }, "geometry": { "type": "Point", "coordinates": [ -77.373, 38.205 ] } },
+{ "type": "Feature", "properties": { "Network Code": "US", "Station Code": "MCWV", "Name": "Mont Chateau, West Virginia, USA", "Latitude": 39.658, "Longitude": -79.846, "Elevation": 280.0 }, "geometry": { "type": "Point", "coordinates": [ -79.846, 39.658 ] } },
+{ "type": "Feature", "properties": { "Network Code": "IU", "Station Code": "SSPA", "Name": "Standing Stone, Pennslyvania", "Latitude": 40.636, "Longitude": -77.888, "Elevation": 270.0 }, "geometry": { "type": "Point", "coordinates": [ -77.888, 40.636 ] } }
+]
+}
